5927dac54de699dfdb1fdfb51c41b1956d3d6be1
Fernando Varella <fernandovarella55@gmail.com>
2020-12-26 20:26:48 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - fernandod c
         </a>
       </header>
     </div>
